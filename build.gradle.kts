da71c56e484bebc99b7ff6e2615dc505eb8eaad2
juan-catalan <juancatalan2001@gmail.com>
2025-03-30 18:13:16 +0200

    First step to update plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,35 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.9.24"
-    id("org.jetbrains.intellij") version "1.17.3"
+    id("org.jetbrains.intellij.platform") version "2.4.0"
+    id("org.jetbrains.intellij.platform.migration") version "2.4.0"
 }
 
 group = "org.juancatalan"
-version = "1.0"
+version = "1.0.2"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2023.2.6")
-    type.set("IC") // Target IDE Platform
+dependencies {
+    intellijPlatform {
+        val type = providers.gradleProperty("platformType")
+        val version = providers.gradleProperty("platformVersion")
 
-    plugins.set(listOf("java", "JUnit"))
+        create(type, version)
+
+        bundledPlugins(listOf("java", "JUnit"))
+        // plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
+    }
 }
 
 tasks {
     // Set the JVM compatibility versions
+
     withType<JavaCompile> {
         sourceCompatibility = "17"
         targetCompatibility = "17"
@@ -29,19 +37,21 @@ tasks {
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions.jvmTarget = "17"
     }
+    /*
+        patchPluginXml {
+            sinceBuild.set("232")
+            untilBuild.set("251.*")
+        }
 
-    patchPluginXml {
-        sinceBuild.set("232")
-        untilBuild.set("242.*")
-    }
+        signPlugin {
+            certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+            privateKey.set(System.getenv("PRIVATE_KEY"))
+            password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+        }
 
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
+        publishPlugin {
+            token.set(System.getenv("PUBLISH_TOKEN"))
+        }
 
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
-    }
+         */
 }
